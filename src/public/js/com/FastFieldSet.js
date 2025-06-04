89aaf26496d33943fc69dc689913852f5b449bcd
andres_alej <127106938+AndresANF@users.noreply.github.com>
2025-06-03 22:57:42 -0400

    feat: Added dynamic title support and responsive layout to FastFieldSet.
    Files updated:
    - FastFieldSet.js: added title logic in render(), removed fixed height assumptions
    - FastFieldSet.css: added .Title styles, removed fixed height, improved spacing
    - FastFieldSet.html: added title prop and more rows for full layout demonstration

--- src/public/js/com/FastFieldSet.js
+++ src/public/js/com/FastFieldSet.js
@@ -1,68 +1,66 @@
-export const FastFieldSet = class extends Fast{
-    constructor(props){
-        super();  
+export const FastFieldSet = class extends Fast {
+    constructor(props) {
+        super();
         this.name = "FastFieldset";
-        this.props = props;     
-        if(props){if(props.id){this.id = props.id}} ;
-        this.built = ()=>{}; 
-        this.attachShadow({mode:'open'});
+        this.props = props;
+        if (props && props.id) this.id = props.id;
+        this.built = () => {};
+        this.attachShadow({ mode: 'open' });
         this._isBuilt = false;
         this.objectProps = new Map();
     }
 
-    #getTemplate(){ return `
-            <div class = "FieldSet">
-                <div class = "Row"></div>
+    #getTemplate() {
+        return `
+            <div class="FieldSet">
+                <div class="Title"></div>
+                <div class="Row"></div>
             </div>
-        `    
+        `;
     }
 
-    async #getCss(){ 
+    async #getCss() {
         return await fast.getCssFile("FastFieldSet");
     }
 
-   #checkAttributes() {
+    #checkAttributes() {
         return new Promise((resolve, reject) => {
             try {
                 for (let attr of this.getAttributeNames()) {
-                    // Ignora atributos que empiecen con "on" (eventos en línea)
                     if (!attr.startsWith('on')) {
-                        // Sincroniza el atributo con la propiedad
                         this[attr] = this.getAttribute(attr);
                     }
-                    // Si es "id", registra el componente en fast
                     if (attr === 'id') {
                         fast.createInstance('FastFieldSet', { 'id': this[attr] });
                     }
                 }
-                resolve(this); // Todo correcto
+                resolve(this);
             } catch (e) {
-                reject(e); // Hubo un error
+                reject(e);
             }
         });
     }
 
-
     #checkProps() {
-    return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
             try {
                 if (this.props) {
                     for (let prop in this.props) {
                         switch (prop) {
                             case 'style':
-                                // Aplica los estilos a la clase principal
                                 for (let cssProp in this.props.style) {
                                     this.shadowRoot.querySelector('.FieldSet').style[cssProp] = this.props.style[cssProp];
                                 }
                                 break;
                             case 'events':
-                                // Añade los eventos declarados en props
                                 for (let eventName in this.props.events) {
                                     this.shadowRoot.querySelector('.FieldSet').addEventListener(eventName, this.props.events[eventName]);
                                 }
                                 break;
+                            case 'title':
+                                this.shadowRoot.querySelector('.Title').textContent = this.props[prop];
+                                break;
                             default:
-                                // Otras props las trata como atributos y propiedades
                                 this[prop] = this.props[prop];
                                 this.setAttribute(prop, this.props[prop]);
                                 if (prop === 'id') {
@@ -81,23 +79,16 @@ export const FastFieldSet = class extends Fast{
     #render(css) {
         return new Promise((resolve, reject) => {
             try {
-                // Crea el template HTML
                 this.template = document.createElement('template');
                 this.template.innerHTML = this.#getTemplate();
 
-                // Aplica el CSS como adoptedStyleSheet
                 let sheet = new CSSStyleSheet();
                 sheet.replaceSync(css);
                 this.shadowRoot.adoptedStyleSheets = [sheet];
 
-                // Clona el contenido del template
                 let clone = this.template.content.cloneNode(true);
-
-                // Guarda referencias a elementos importantes
                 this.mainElement = clone.querySelector('.FieldSet');
                 this.rowsContainer = this.mainElement.querySelector('.Row');
-
-                // Inserta en el shadowRoot
                 this.shadowRoot.appendChild(this.mainElement);
 
                 resolve(this);
@@ -107,13 +98,10 @@ export const FastFieldSet = class extends Fast{
         });
     }
 
-    
-
     async connectedCallback() {
         await this.#render(await this.#getCss());
         await this.#checkAttributes();
         await this.#checkProps();
-        //this.#events();
         this._isBuilt = true;
         this.built(this);
     }
@@ -125,7 +113,6 @@ export const FastFieldSet = class extends Fast{
 
     addRowBody({ elements = [], style = {} }, index = null) {
         let row = document.createElement('div');
-        
         row.classList.add('Row');
 
         if (!Array.isArray(elements)) {
@@ -149,11 +136,8 @@ export const FastFieldSet = class extends Fast{
 
         return this;
     }
+};
 
-
-
-}
-
-if (!customElements.get ('fast-fieldset')) {
-    customElements.define ('fast-fieldset', FastFieldSet);
+if (!customElements.get('fast-fieldset')) {
+    customElements.define('fast-fieldset', FastFieldSet);
 }
