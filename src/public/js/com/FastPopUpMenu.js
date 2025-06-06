c1739c82afa5225430de2aeb4bf0cfa7d55db3e0
reyesrra <reyesrainny11@gmail.com>
2025-06-06 07:46:04 -0400

    update: component's life cycle implemented

--- src/public/js/com/FastPopUpMenu.js
+++ src/public/js/com/FastPopUpMenu.js
@@ -1,21 +1,121 @@
-class FastPopUpMenu extends HTMLElement {
-    constructor(){
+export const FastPopUpMenu = class extends Fast {
+    constructor(props) {
         super();
-        this.attachShadow({mode: "open"});
-
-        const linkElem = document.createElement('link');
-        linkElem.setAttribute('rel', 'stylesheet');
-        linkElem.setAttribute('href', '../js/css/FastPopUpMenu.css');
-        this.shadowRoot.appendChild(linkElem);
-
-        const menuDiv = document.createElement('div');
-        menuDiv.id = 'fast-popup-menu';
-        const ul = document.createElement('ul');
-        menuDiv.appendChild(ul);
-        this.shadowRoot.appendChild(menuDiv);
+        this.name = "FastPopUpMenu";
+        this.props = props;
+        if (props && props.id) this.id = props.id;
+        this.built = () => {};
+        this.attachShadow({ mode: "open" });
+        this._isBuilt = false;
+        this.objectProps = new Map();
     }
 
-    addItem(text, callback){
+    #getTemplate() {
+        return `
+            <div id="fast-popup-menu">
+                <ul></ul>
+            </div>
+        `;
+    }
+
+    async #getCss() {
+        return await fast.getCssFile("FastPopUpMenu");
+    }
+
+    #render() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                let sheet = new CSSStyleSheet();
+                let css = await this.#getCss();
+                sheet.replaceSync(css);
+                this.shadowRoot.adoptedStyleSheets = [sheet];
+                this.template = document.createElement('template');
+                this.template.innerHTML = this.#getTemplate();
+                let tpc = this.template.content.cloneNode(true);
+                this.mainElement = tpc.firstChild.nextSibling;
+                this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+                resolve(this);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    #checkAttributes() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                for (let attr of this.getAttributeNames()) {
+                    if (attr.substring(0, 2) !== "on") {
+                        this[attr] = this.getAttribute(attr);
+                    }
+                    switch (attr) {
+                        case 'id':
+                            await fast.createInstance('FastPopUpMenu', { 'id': this[attr] });
+                            break;
+                    }
+                }
+                resolve(this);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    #checkProps() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (this.props) {
+                    for (let attr in this.props) {
+                        switch (attr) {
+                            case 'style':
+                                for (let attrcss in this.props.style)
+                                    this.mainElement.style[attrcss] = this.props.style[attrcss];
+                                break;
+                            case 'events':
+                                for (let attrevent in this.props.events)
+                                    this.mainElement.addEventListener(attrevent, this.props.events[attrevent]);
+                                break;
+                            default:
+                                this.setAttribute(attr, this.props[attr]);
+                                this[attr] = this.props[attr];
+                                if (attr === 'id')
+                                    await fast.createInstance('FastPopUpMenu', { 'id': this[attr] });
+                        }
+                    }
+                    resolve(this);
+                } else {
+                    resolve();
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    async connectedCallback() {
+        await this.#render();
+        await this.#checkAttributes();
+        await this.#checkProps();
+        this._isBuilt = true;
+        await this.#applyProps();
+        this.built();
+    }
+
+    #applyProps() {
+        return new Promise((resolve, reject) => {
+            try {
+                for (const [key, value] of this.objectProps.entries()) {
+                    this[key] = value;
+                    this.objectProps.delete(key);
+                }
+                resolve(this);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    addItem(text, callback) {
         const ul = this.shadowRoot.querySelector('ul');
         const li = document.createElement('li');
         li.textContent = text;
@@ -23,17 +123,22 @@ class FastPopUpMenu extends HTMLElement {
         ul.appendChild(li);
     }
 
-    show(event){
+    show(event) {
         const popupMenu = this.shadowRoot.querySelector('#fast-popup-menu');
         popupMenu.style.top = `${event.clientY}px`;
         popupMenu.style.left = `${event.clientX}px`;
         popupMenu.style.display = 'block';
+        popupMenu.style.zIndex = fast.getMaxZIndex();
     }
 
-    hide(){
+    hide() {
         const popupMenu = this.shadowRoot.querySelector('#fast-popup-menu');
         popupMenu.style.display = 'none';
     }
+
+    addToBody() { document.body.appendChild(this); }
 }
 
-customElements.define('fast-popup-menu', FastPopUpMenu);
+if (!customElements.get('fast-popup-menu')) {
+    customElements.define('fast-popup-menu', FastPopUpMenu);
+}
