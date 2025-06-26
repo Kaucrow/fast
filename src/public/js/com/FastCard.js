a0318d2cda59298c54d6e471fe45a6e9740e1cdc
Ejempr03 <erick.31085525@uru.edu>
2025-06-26 11:44:45 -0400

    Add atributo layout
    Se agrego el atributo layout para que sea mas facil y personalizable la card

--- src/public/js/com/FastCard.js
+++ src/public/js/com/FastCard.js
@@ -1,5 +1,6 @@
 export const FastCard = class extends Fast {
-    static observedAttributes = ["title", "content", "img"];
+    // 1. Añadimos 'layout' a los atributos observados
+    static observedAttributes = ["title", "content", "img", "layout"];
 
     constructor(props) {
         super();
@@ -16,23 +17,54 @@ export const FastCard = class extends Fast {
         return `
             <div class="FastCard">
                 <img class="card-img" style="display: none;" />
-                <h3 class="card-title"></h3>
-                <div class="card-content"></div>
+                <div class="card-text-wrapper">
+                    <h3 class="card-title"></h3>
+                    <div class="card-content"></div>
+                </div>
             </div>
         `;
     }
 
     async #getCss() {
-        let baseCss = await fast.getCssFile("FastCard");
-        const additionalCss = `
-            .card-img[hidden] {
-                display: none !important;
+        let baseCss = await fast.getCssFile("FastCard"); 
+        const layoutCss = `
+            .FastCard {
+                display: flex;
+                gap: 1em;
             }
-            .card-img:not([src]), .card-img[src=""] {
+            .card-text-wrapper {
+                display: flex;
+                flex-direction: column;
+                gap: 0.5em;
+            }
+            /* Layout por defecto (imagen arriba) */
+            .FastCard, .FastCard[layout="image-top"] {
+                flex-direction: column;
+            }
+            /* Layout con imagen abajo */
+            .FastCard[layout="image-bottom"] {
+                flex-direction: column;
+            }
+            .FastCard[layout="image-bottom"] .card-img {
+                order: 1; /* Mueve la imagen al final */
+            }
+            /* Layout con imagen a la izquierda */
+            .FastCard[layout="image-left"] {
+                flex-direction: row;
+                align-items: center;
+                text-align: left;
+            }
+            /* Layout con imagen a la derecha */
+            .FastCard[layout="image-right"] {
+                flex-direction: row-reverse;
+                align-items: center;
+                text-align: right;
+            }
+            .card-img[hidden], .card-img:not([src]), .card-img[src=""] {
                 display: none !important;
             }
         `;
-        return baseCss + additionalCss;
+        return baseCss + layoutCss;
     }
 
     #render() {
@@ -47,6 +79,8 @@ export const FastCard = class extends Fast {
                 this.template.innerHTML = this.#getTemplate();
                 let tpc = this.template.content.cloneNode(true); 
                 this.shadowRoot.appendChild(tpc);
+                
+                // Enlazamos los elementos internos
                 this.mainElement = this.shadowRoot.querySelector('.FastCard');
                 this.titleElement = this.shadowRoot.querySelector('.card-title');
                 this.contentElement = this.shadowRoot.querySelector('.card-content');
@@ -72,13 +106,13 @@ export const FastCard = class extends Fast {
         }
     }
 
+    // Actualizamos #checkAttributes para que maneje el atributo 'layout'
     #checkAttributes() {
         return new Promise(async (resolve, reject) => {
             try {
                 for (let attr of this.getAttributeNames()) {
                     if (attr.substring(0, 2) !== "on") {
                         this[attr] = this.getAttribute(attr);
-                        this.setAttribute(attr, this.getAttribute(attr));
                         switch(attr){
                             case 'title':
                                 if (this.titleElement) this.titleElement.innerText = this[attr];
@@ -88,6 +122,9 @@ export const FastCard = class extends Fast {
                                 break;
                             case 'img':
                                 this.#updateImageVisibility(this[attr]);
+                                break;
+                            case 'layout':
+                                if (this.mainElement) this.mainElement.setAttribute('layout', this[attr]);
                                 break;
                             case 'id':
                                 await fast.createInstance('FastCard', { id: this[attr] });
@@ -107,30 +144,17 @@ export const FastCard = class extends Fast {
             try {
                 if (this.props) {
                     for (let attr in this.props) {
+                        this[attr] = this.props[attr]; 
                         switch (attr) {
                             case 'style':
-                                for (let k in this.props.style)
-                                    this.mainElement.style[k] = this.props.style[k];
+                                for (let k in this.props.style) this.mainElement.style[k] = this.props.style[k];
                                 break;
                             case 'events':
-                                for (let e in this.props.events)
-                                    this.mainElement.addEventListener(e, this.props.events[e]);
+                                for (let e in this.props.events) this.mainElement.addEventListener(e, this.props.events[e]);
                                 break;
-                            case 'title': 
-                            case 'content':
+                            case 'layout':
                                 this.setAttribute(attr, this.props[attr]);
-                                this[attr] = this.props[attr];
                                 break;
-                            case 'img':
-                                this.setAttribute(attr, this.props[attr]);
-                                this[attr] = this.props[attr];
-                                this.#updateImageVisibility(this.props[attr]);
-                                break;
-                            default:
-                                this.setAttribute(attr, this.props[attr]);
-                                this[attr] = this.props[attr];
-                                if (attr === 'id')
-                                    await fast.createInstance('FastCard', { id: this[attr] });
                         }
                     }
                 }
@@ -146,59 +170,31 @@ export const FastCard = class extends Fast {
         await this.#checkAttributes();
         await this.#checkProps();
         this._isBuilt = true;
-        await this.#applyProps();
         this.built();
     }
-
-    #applyProps() {
-        return new Promise((resolve, reject) => {
-            try {
-                for (const [key, value] of this.objectProps.entries()) {
-                    if (key === 'img') {
-                        this.#updateImageVisibility(value);
-                    } else {
-                        this[key] = value;
-                    }
-                    this.objectProps.delete(key);
-                }
-                resolve(this);
-            } catch (e) {
-                reject(e);
-            }
-        });
+    
+    get layout() { return this.getAttribute('layout'); }
+    set layout(val) {
+        this.setAttribute('layout', val);
+        if (this.mainElement) {
+            this.mainElement.setAttribute('layout', val);
+        }
     }
 
-    addToBody() {
-        document.body.appendChild(this);
-    }
-
-    get title() {
-        return this.titleElement?.innerText || "";
-    }
+    get title() { return this.titleElement?.innerText || ""; }
     set title(val) {
         this.setAttribute("title", val);
         if (this.titleElement) this.titleElement.innerText = val;
-        else this.objectProps.set("title", val);
     }
-
-    get content() {
-        return this.contentElement?.innerText || "";
-    }
+    get content() { return this.contentElement?.innerText || ""; }
     set content(val) {
         this.setAttribute("content", val);
         if (this.contentElement) this.contentElement.innerText = val;
-        else this.objectProps.set("content", val);
     }
-
-    get img() {
-        return this.imgElement?.src || "";
-    }
+    get img() { return this.imgElement?.src || ""; }
     set img(val) {
         this.setAttribute("img", val);
         this.#updateImageVisibility(val);
-        if (!this.imgElement) {
-            this.objectProps.set("img", val);
-        }
     }
 };
 
